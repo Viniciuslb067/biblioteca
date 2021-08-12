b46b6d700076cb34c59beac0ff61f0a50ce182b3
Viniciuslb <viniciuslb067@gmail.com>
2021-08-12 20:12:11 -0300

    feat: resources

---
+++ backend/app/Http/Resources/AuthorResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthorResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'author_name' => $this->author_name,
+            'birthDate' => $this->birthDate,
+            'sex' => $this->sex
+        ];
+    }
+}
