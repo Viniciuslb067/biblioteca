38bf37c3ec655f43acd3577982a8fc2ecbb26a74
Viniciuslb067 <viniciuslb067@gmail.com>
2021-08-13 13:39:21 -0300

    feat: routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import { Index } from "./pages/Index";
 import { Home } from "./pages/Home";
+import { Books } from "./pages/Books";
+
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import { SidebarProvider } from "./contexts/SidebarContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} /> 
-      </Switch>
+      <SidebarProvider>
+        <Header />
+        <Sidebar />
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/books" exact component={Books} />
+        </Switch>
+      </SidebarProvider>
     </BrowserRouter>
   );
 }
